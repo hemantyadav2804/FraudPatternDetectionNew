bee22cc8da628215b659914198fde2f04e5121b9
Rohit Patil <ro.mo.643216@gmail.com>
2026-01-07 18:32:43 +0530

    created all nacessary packeges in src/main/java

---
+++ src/test/java/com/bank/frauddetection/FraudDetectionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.frauddetection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
